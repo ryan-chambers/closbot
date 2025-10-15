157a70913b8b72398cf33efd47c87d73cd7283a6
Ryan Chambers <ryan.b.chambers@gmail.com>
2025-10-15 19:54:51 -0400

    Cite sources (#26)

--- src/app/services/prompt-helper.util.ts
+++ src/app/services/prompt-helper.util.ts
@@ -2,13 +2,17 @@ import { WineContext } from '../models/wines.model';
 
 const chatSystemPrompt = `You are a sommelier with an expertise in wine from Burgundy. You are acting as my personal assistant. You will help answer any questions or comments about wine. Please return text with only markdown formatting.`;
 
+function combineStrings(reviews: string[] | undefined): string {
+  return (reviews ?? []).join(', ');
+}
+
 export const createChatSystemPrompt = (wineContext: WineContext): string => {
   let prompt = chatSystemPrompt;
   if (wineContext.otherContext.length > 0) {
-    prompt += `\n\nYou have access to some of my additional notes: ${wineContext.otherContext.join(', ')}. Make sure to include it if it is relevant.`;
+    prompt += `\n\nYou have access to some of my additional notes: ${combineStrings(wineContext.otherContext)}. Make sure to include it if relevant. Also be sure to mention the source.`;
   }
   if (wineContext.personalReviews.length > 0) {
-    prompt += `\n\nYou have access to some of my personal reviews: ${wineContext.personalReviews.join(', ')}. Make sure to include it if it is relevant.`;
+    prompt += `\n\nHere are some of my personal reviews. Make use of them if appropriate: ${combineStrings(wineContext.personalReviews)}`;
   }
   return prompt;
 };
@@ -18,10 +22,10 @@ const menuSummarySystemPrompt = `You are a sommelier with expertise in wine from
 export const createMenuSummarySystemPrompt = (wineContext: WineContext): string => {
   let prompt = menuSummarySystemPrompt;
   if (wineContext.otherContext.length > 0) {
-    prompt += `\n\nYou have access to some of my additional notes: ${wineContext.otherContext.join(', ')}. Make sure to include it if it is relevant.`;
+    prompt += `\n\nYou have access to some of my additional notes: ${combineStrings(wineContext.otherContext)}. Make sure to include it if relevant. Also be sure to mention the source.`;
   }
   if (wineContext.personalReviews.length > 0) {
-    prompt += `\n\nYou have access to some of my personal reviews: ${wineContext.personalReviews.join(', ')}. Make sure to use consider them when making a recommendation.`;
+    prompt += `\n\nHere are some of my personal reviews. Make sure to use consider them when making a recommendation. ${combineStrings(wineContext.personalReviews)}`;
   }
   return prompt;
 };
